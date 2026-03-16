15d1703467337a4cd515e121b6d0217e3913c939
Matheus Carvalho <117946744+mattprogrammer@users.noreply.github.com>
2026-03-16 19:13:35 -0300

    Registro de usuário com validação de email

---
+++ src/TechChallengeFase1.Domain/DTOs/UserRegistrationInputDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace TechChallengeFase1.Domain.DTOs
+{
+    public class UserRegistrationInputDto
+    {
+        [Required(ErrorMessage = "O E-mail é obrigatório.")]
+        [EmailAddress(ErrorMessage = "O E-mail deve ser um endereço de e-mail válido.")]
+        public required string Email { get; set; }
+        public required string Username { get; set; }
+        public required string Password { get; set; }
+        public required DateTime DateOfBirth { get; set; }
+        public required string DocumentNumber { get; set; }
+    }
+}
