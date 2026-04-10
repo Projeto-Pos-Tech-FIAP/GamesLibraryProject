e41d9f3037e3fa5554e40cda428407839f12bfac
Matheus Carvalho <mh@zitec.ai>
2026-04-10 15:43:43 -0300

    Marker de entidade e repository padrão para abstração

---
+++ src/TechChallengeFase1.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace TechChallengeFase1.Domain.Interfaces;
+
+public interface IRepository<T> where T : IEntity
+{
+    Task<T> GetByIdAsync(int id);
+    Task<IEnumerable<T>> GetAllAsync();
+    Task AddAsync(T entity);
+    Task UpdateAsync(T entity);
+    Task DeleteAsync(int id);
+}
